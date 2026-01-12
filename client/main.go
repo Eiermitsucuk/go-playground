c1a39959a212037867efb25d708dc1ee3e97f70a
Halil Cankilic <141903706+Eiermitsucuk@users.noreply.github.com>
2026-01-12 12:48:12 +0100

    header

--- client/main.go
+++ client/main.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:8080/")
+	req, err := http.NewRequest("GET", "http://localhost:8080/", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
